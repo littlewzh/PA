b221e7778bb62cf265f7b86ec5bba3efa21f005a
tracer-ics2021 <tracer@njuics.org>
2021-09-26 18:07:32 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5818a4b866a949cd9db54a0f2b1b8123   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  18:07:32 up  1:48,  1 user,  load average: 0.56, 0.46, 0.47

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -5,7 +5,8 @@
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-
+  char str[64];                      //表达式
+  uint32_t val;                       //表达式的结果
   /* TODO: Add more members if necessary */
 
 } WP;
@@ -25,5 +26,53 @@ void init_wp_pool() {
   free_ = wp_pool;
 }
 
+WP* new_wp(char *s){
+   if(free_==NULL) assert(0);
+   bool success;
+   if(head==NULL){
+     head=free_;
+     free_=free_->next;
+     head->next=NULL;
+     strcpy(head->str,s);
+     head->val=expr(s,&success);
+     return head;
+   }
+   else {
+     WP* p=head;
+     while(p->next){
+       p=p->next;
+     }
+     p->next=free_;
+     free_=free_->next;
+     p=p->next;
+     p->next=NULL;
+     strcpy(p->str,s);
+     p->val=expr(s,&success);
+     return p;
+   }
+}
+void free_wp(WP *wp){
+  WP *current;
+  WP *p;
+  if(wp==head) head=head->next;
+  else {
+    current=head;
+    while((current->next!=wp)&&current!=NULL){
+      current=current->next;
+    }
+    if(current==NULL) {printf("没有此监视点"); return;}
+    else {
+      p=wp->next;
+      current->next=p;
+    }
+  }
+  while(current)
+  wp->val=0;
+  memset(wp->str,'\0',sizeof(wp->str));
+  wp->next=free_;
+  free_=wp;
+  free_->next=p;
+  return;
+}
 /* TODO: Implement the functionality of watchpoint */
 
