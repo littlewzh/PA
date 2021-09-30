56cd0c9020520d01f9414168f66aaeba7613bec6
tracer-ics2021 <tracer@njuics.org>
2021-09-30 20:28:44 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: c2b701f20311448796a0abe68fbd7f52   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:28:45 up  5:58,  1 user,  load average: 0.20, 0.14, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -193,8 +193,8 @@ int find_main_operator(int p,int q){          //寻找主操作符
 }
 uint32_t eval(int p,int q){
   if(p>q){
-    assert(0);
-    //return 0;                             //这个地方（）里是0还是1还要在思考一下？
+    //assert(0);
+    return 0;                             //这个地方（）里是0还是1还要在思考一下？
   }
   else if(p==q){                          //此处应进行更加详细的分类，区别十进制，十六进制，寄存器的值
     if(tokens[p].type==TK_NUM){
@@ -223,7 +223,7 @@ uint32_t eval(int p,int q){
       bool success;
       return   isa_reg_str2val(s,&success);
     }
-    //else assert(0);//if(tokens[p].type==NEG) {return }
+    else {assert(0);}//if(tokens[p].type==NEG) {return }
   }
   else if(check_parentheses(p,q)==true){
     return eval(p+1,q-1);
