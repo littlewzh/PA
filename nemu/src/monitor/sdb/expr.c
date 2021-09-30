e4a7141361e93b26a30be631b1723bc5138f0f2d
tracer-ics2021 <tracer@njuics.org>
2021-09-30 17:40:57 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: c2b701f20311448796a0abe68fbd7f52   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  17:40:57 up  3:11,  1 user,  load average: 0.17, 0.08, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -262,7 +262,7 @@ word_t expr(char *e,bool *success) {
   tokens[i].type=DEREF;
 }  
   if(tokens[i].type=='-'&&(i==0||(tokens[i-1].type!=TK_NUM&&tokens[i-1].type!=TK_HEX&&tokens[i-1].type!=TK_REG&&tokens[i-1].type!=')'))){
-  tokens[i].type=NEG;//Log("%d",i);
+  tokens[i].type=NEG;Log("%d",tokens[i].type);
 }
 }  
 
