aa86e109400924de82a39900ebbb2739ad505b1c
tracer-ics2021 <tracer@njuics.org>
2021-10-11 17:19:53 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: b43b39dc7a8e47539ba1ae3598dce646   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  17:19:54 up  1:15,  1 user,  load average: 0.11, 0.16, 0.22

--- nemu/src/isa/riscv32/instr/contral.h
+++ nemu/src/isa/riscv32/instr/contral.h
@@ -3,7 +3,7 @@ def_EHelper(jal) {
   rtl_addi(s,ddest,s0,s->pc);
   rtl_li(s,s1,id_src1->imm);
   rtl_sext(s,s2,s1,4);
-  s->dnpc=(s->snpc)+(*s2);
+  s->dnpc=(s->pc)+(*s2);
 }
 def_EHelper(ret) {
   s->dnpc=cpu.gpr[1]._32;
