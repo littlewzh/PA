a5d72ac6ee73a81da85a0f19a95345093a77af15
tracer-ics2021 <tracer@njuics.org>
2021-10-13 15:21:42 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 1f54c2b0618c41b6b77dcf1ff17b2be7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  15:21:42 up 28 min,  1 user,  load average: 0.28, 0.29, 0.29

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -5,3 +5,10 @@ def_EHelper(beq){
         s->dnpc=(s->pc)+(*t0);
     }
 }
+def_EHelper(bne){
+    if(*dsrc1!=*dsrc2){
+         rtl_li(s,t0,id_dest->imm);
+        rtl_sext(s,t0,t0,12);
+        s->dnpc=(s->pc)+(*t0);
+    }
+}
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -5,8 +5,8 @@ def_EHelper(addi) {
   rtl_addi(s,ddest,dsrc1,id_src2->imm);
 }
 def_EHelper(auipc) {
-  rtl_li(s,s0,id_src1->imm);
-  rtl_addi(s,ddest,s0,s->pc);
+  rtl_li(s,t0,id_src1->imm);
+  rtl_addi(s,ddest,t0,s->pc);
 }
 def_EHelper(add) {
   rtl_add(s,ddest,dsrc1,dsrc2);
@@ -15,4 +15,10 @@ def_EHelper(sub) {
   rtl_not(s,dsrc2,dsrc2);
   rtl_addi(s,dsrc2,dsrc2,1);
   rtl_add(s,ddest,dsrc1,dsrc2);
+}
+def_EHelper(sltiu) {
+  if(*dsrc1<id_src2->imm) {
+    rtl_li(s,ddest,1);
+  }
+  else {rtl_li(s,ddest,0);}
 }
