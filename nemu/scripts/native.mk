4b13c28a6ebf82d4d723736c13c9bd16d435c2e0
tracer-ics2021 <tracer@njuics.org>
2021-10-13 16:54:07 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 1f54c2b0618c41b6b77dcf1ff17b2be7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:54:07 up  2:00,  1 user,  load average: 0.41, 0.39, 0.32

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -14,7 +14,7 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
-IMG := /am-kernels/tests/cpu-tests/tests/add.c
+IMG := /am-kernels/tests/cpu-tests/build/add-riscv32-nemu
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
