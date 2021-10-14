8319c46a75a10cb57515b5f382a52d63e4f7e2d3
tracer-ics2021 <tracer@njuics.org>
2021-10-14 11:06:48 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5930518043c24359bfdeb3f033f1861d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:06:48 up 46 min,  1 user,  load average: 0.45, 0.38, 0.49

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -7,7 +7,7 @@ def_EHelper(beq){
 }
 def_EHelper(bne){
     if(*dsrc1!=*dsrc2){
-         rtl_li(s,t0,id_dest->imm);
+        rtl_li(s,t0,id_dest->imm);
         rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(*t0);
     }
--- nemu/src/isa/riscv32/instr/contral.h
+++ nemu/src/isa/riscv32/instr/contral.h
@@ -1,6 +1,6 @@
 def_EHelper(jal) {
   rtl_li(s,ddest,id_src1->imm);
-  rtl_sext(s,ddest,ddest,20);
+  //rtl_sext(s,ddest,ddest,20);
   s->dnpc=(s->pc)+(*ddest);
   rtl_addi(s,ddest,rz,(s->pc)+4);
 }
