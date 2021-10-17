dd629bf09648c781ee72c6faba8bb9db59aa29d3
tracer-ics2021 <tracer@njuics.org>
2021-10-17 09:07:08 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 026c4b638cfc47a5bb775e12281f7eb5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  09:07:08 up 59 min,  1 user,  load average: 0.68, 0.40, 0.25

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -26,7 +26,8 @@ void sdb_set_batch_mode();
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
-static char *img_file = NULL;
+static char *img_file = "/home/wang/ics2021/am-kernels/tests/cpu-tests/build/string-riscv32-nemu.bin";
+//NULL;
 //"/home/wang/ics2021/am-kernels/tests/cpu-tests/tests/dummy.c";
 //"add";
 static int difftest_port = 1234;
