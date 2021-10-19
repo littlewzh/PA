2960542bbf4b6b9c6692824d9c7eea4de8dabc57
tracer-ics2021 <tracer@njuics.org>
2021-10-20 01:01:28 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 96c5d7e0da6e46a9979e7cb31a5d9a6b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  01:01:28 up  1:01,  1 user,  load average: 0.32, 0.39, 0.37

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -47,7 +47,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) { 
+    Log("memory write  at pc=0x%08x",cpu.pc);
+    pmem_write(addr, len, data); return; }
   MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
       addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
