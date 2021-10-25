79ab9774f36d2370ac8ad931930166fef35acd0b
tracer-ics2021 <tracer@njuics.org>
2021-10-26 00:43:38 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 8e26320cb0ae4173b51474cd7df4d673   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:43:38 up  2:13,  1 user,  load average: 0.35, 0.46, 0.46

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,12 +1,26 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <amdev.h>
+#include <sys/time.h>
+#include <time.h>
+static struct timeval boot_time = {};
+void __am_timer_config(AM_TIMER_CONFIG_T *cfg) {
+  cfg->present = cfg->has_rtc = true;
+}
 void __am_timer_init() {
+gettimeofday(&boot_time, NULL);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = 0;
+  struct timeval now;
+  gettimeofday(&now,NULL);
+  //clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
+  long seconds = now.tv_sec - boot_time.tv_sec;
+  long useconds = now.tv_usec - boot_time.tv_usec;
+  uptime->us = seconds * 1000000 + (useconds + 500);
 }
+  //now.tv_sec * 1000000 + now.tv_nsec / 1000;
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
