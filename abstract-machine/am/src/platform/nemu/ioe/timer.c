f0b2a94f92b175e368349258741c03c456aa0197
tracer-ics2021 <tracer@njuics.org>
2021-10-26 17:33:59 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: bf2267860e104c95a506298c0ad7ae8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  17:33:59 up 52 min,  1 user,  load average: 0.75, 0.68, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -14,7 +14,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   //uptime->us = 0;
-  uptime->us+=100;
+  uptime->us+=500;
  
   /*struct timeval now;
   gettimeofday(&now,NULL);
