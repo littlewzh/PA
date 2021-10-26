c1c053345ca614f8941b1fd3ec2c9d3ddc41bbc8
tracer-ics2021 <tracer@njuics.org>
2021-10-26 16:55:05 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: bf2267860e104c95a506298c0ad7ae8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:55:05 up 13 min,  1 user,  load average: 0.47, 0.34, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,23 +4,25 @@
 #include <sys/time.h>
 //#include <time.h>
 #include<unistd.h>
-/*int gettimeofday(struct timeval *tv, void *tz);
-static struct timeval boot_time = {};
-void __am_timer_config(AM_TIMER_CONFIG_T *cfg) {
-  cfg->present = cfg->has_rtc = true;
-}
+//int gettimeofday(struct timeval *tv, void *tz);
+//static struct timeval boot_time = {};
+
 void __am_timer_init() {
-gettimeofday(&boot_time, NULL);
+
+//gettimeofday(&boot_time, NULL);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = 0;
-  struct timeval now;
+  while(1){
+    uptime->us++;
+  }
+  /*struct timeval now;
   gettimeofday(&now,NULL);
   //clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
   long seconds = now.tv_sec - boot_time.tv_sec;
   long useconds = now.tv_usec - boot_time.tv_usec;
-  uptime->us = seconds * 1000000 + (useconds + 500);
+  uptime->us = seconds * 1000000 + (useconds + 500);*/
 }
   //now.tv_sec * 1000000 + now.tv_nsec / 1000;
 
@@ -31,32 +33,5 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->day    = 0;
   rtc->month  = 0;
   rtc->year   = 1900;
-}*/
-static uint64_t boot_time = 0;
-
-#define CLINT_MMIO 0x2000000ul
-#define TIME_BASE 0xbff8
-
-static uint64_t read_time() {
-  uint32_t lo = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 0);
-  uint32_t hi = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 4);
-  uint64_t time = ((uint64_t)hi << 32) | lo;
-  return time / 10;
 }
 
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = read_time() - boot_time;
-}
-
-void __am_timer_init() {
-  boot_time = read_time();
-}
-
-void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
-}
