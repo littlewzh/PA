323809f0ad55dabafc66745594b8feedbb51024f
tracer-ics2021 <tracer@njuics.org>
2021-10-27 12:00:14 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f80596af78f84113a6895ea103850f7b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  12:00:14 up  2:48,  1 user,  load average: 1.00, 0.59, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,8 +3,13 @@
 
 #define KEYDOWN_MASK 0x8000
 #define kaddr 0xa0000060
+uint64_t k;
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-
-  kbd->keydown = (inl(kaddr)& KEYDOWN_MASK ? false: true);
-  kbd->keycode = inl(kaddr)&~KEYDOWN_MASK;
+  k=inl(kaddr);
+  kbd->keycode=k;
+  if(kbd->keycode!=AM_KEY_NONE){
+      kbd->keydown=!(kbd->keydown);
+  }
+  //kbd->keydown = (inl(kaddr)& KEYDOWN_MASK ? false: true);
+  //kbd->keycode = inl(kaddr)&~KEYDOWN_MASK;
 }
