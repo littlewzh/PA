fb0b42b3e2d982f09a597bcb07c19bcc50152ba9
tracer-ics2021 <tracer@njuics.org>
2021-10-27 21:44:08 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f33883a3603f4ace8733c1bab5ee1f43   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:44:08 up 17 min,  1 user,  load average: 0.56, 0.39, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,20 +2,21 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
+static int size;
 void __am_gpu_init() {
+  size=inl(VGACTL_ADDR);
   int i;
-  int w = (uint32_t)inw(VGACTL_ADDR+2);  // TODO: get the correct width
-  int h = (uint32_t)inw(VGACTL_ADDR);  // TODO: get the correct height
+  int w = (size&((1<<16)-1))>>16;  // TODO: get the correct width
+  int h = (size<<16)>>16;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  outl(SYNC_ADDR,inl(SYNC_ADDR));
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = (uint32_t)inw(VGACTL_ADDR+2), .height = (uint32_t)inw(VGACTL_ADDR),
+    .width = (size&((1<<16)-1))>>16, .height = (size<<16)>>16,
     .vmemsz = 0
   };
 }
