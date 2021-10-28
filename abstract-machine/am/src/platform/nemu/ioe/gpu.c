4c2525a91e5f8d25c3b1b652c32a562d2d223693
tracer-ics2021 <tracer@njuics.org>
2021-10-28 09:56:22 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 7a1fb0c70c484002b12242658147984b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  09:56:22 up 47 min,  1 user,  load average: 1.08, 0.74, 0.51

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,11 +8,11 @@ int min(int a, int b){
 	else return b;
 }
 //static uint32_t* fb =(uint32_t *)(uintptr_t)FB_ADDR;
-static int W;
-static int H;
+int W=400;//(int)inw(VGACTL_ADDR+2);
+int H=300;//(int)inw(VGACTL_ADDR);
 void __am_gpu_init() {
-  W=(int)inw(VGACTL_ADDR+2);
-  H=(int)inw(VGACTL_ADDR);
+  //W=(int)inw(VGACTL_ADDR+2);
+  //H=(int)inw(VGACTL_ADDR);
   /*int i;
   //int w = W;  // TODO: get the correct width
   //int h = H;  // TODO: get the correct height
@@ -31,14 +31,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
-    /*int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
-    //uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    uint32_t *pixels = ctl->pixels;
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    //for (i = 0; i < W* H; i ++) fb[i] = i;
-    for(int j = 0; j < h  && y + j < H; ++j){
-      outl(SYNC_ADDR, *pixels);
-      pixels+=w;*/
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
 			uint32_t *pixels = ctl->pixels;
 			//int cp_bytes = sizeof(uint32_t) * min(w, W - x);
