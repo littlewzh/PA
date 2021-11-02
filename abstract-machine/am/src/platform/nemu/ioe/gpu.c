8fb1d506261a176389ffeca04d3b692c7506edce
tracer-ics2021 <tracer@njuics.org>
2021-11-02 22:05:56 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: a083211dd40243acb123252fa1573143   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:05:56 up 21 min,  1 user,  load average: 0.36, 0.22, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,10 +30,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
 	uint32_t *pixels = ctl->pixels;
-	for(int j=0; j<h&&y+j<H;++j){
-    for(int i=0;i+x<W&&i<w;++i){
+	for(uint32_t j=0; j<h&&y+j<H;++j){
+    for(uint32_t i=0;i+x<W&&i<w;++i){
       outl(IMG+((y+j)*W + x+i)*4,*pixels);
       pixels ++;
     }
