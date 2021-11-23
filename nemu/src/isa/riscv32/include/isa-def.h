fb825be486ffd3ebc09e17937f0f0d0d725ec91d
tracer-ics2021 <tracer@njuics.org>
2021-11-23 18:48:30 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 9e8b1a07ffbc404c949e55cb332fef95   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  18:48:30 up 34 min,  1 user,  load average: 1.24, 0.98, 0.91

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -69,7 +69,7 @@ typedef struct {
       uint32_t imm10_5   : 6;
       int32_t  simm12    : 1;
     } b;
-     struct {
+    struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
       uint32_t rd        : 5;
