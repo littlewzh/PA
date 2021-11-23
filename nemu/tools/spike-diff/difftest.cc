6ddfb029e872cfd7a7502e910a45fc2c8ac319d6
tracer-ics2021 <tracer@njuics.org>
2021-11-24 00:45:46 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: b2090b4ddbfd403c878c6a5c9b4eeb7a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:45:46 up  2:40,  1 user,  load average: 0.52, 0.42, 0.33

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -26,10 +26,10 @@ static debug_module_config_t difftest_dm_config = {
 struct diff_context_t {
   word_t gpr[32];
   word_t pc;
-  //rtlreg_t mstatus;
-  //rtlreg_t mepc;
-  //rtlreg_t mcause;
-  //rtlreg_t mtvec;
+  rtlreg_t mstatus;
+  rtlreg_t mepc;
+  rtlreg_t mcause;
+  rtlreg_t mtvec;
 };
 
 static sim_t* s = NULL;
