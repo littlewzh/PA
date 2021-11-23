e5c2629526f7b0f8b240c67cd6d6acbe8fc66db4
tracer-ics2021 <tracer@njuics.org>
2021-11-23 20:51:13 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 9e8b1a07ffbc404c949e55cb332fef95   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:51:14 up  2:37,  1 user,  load average: 1.14, 1.28, 1.26

--- abstract-machine/am/src/riscv/mycpu/cte.c
+++ abstract-machine/am/src/riscv/mycpu/cte.c
@@ -10,6 +10,8 @@ Context* __am_irq_handle(Context *c) {
       case 1:ev.event = EVENT_YIELD; break;
       case 2:ev.event = EVENT_SYSCALL;break;
       case 3:ev.event = EVENT_PAGEFAULT; break;
+      case 5:ev.event = EVENT_IRQ_TIMER;break;
+      case 6:ev.event = EVENT_IRQ_IODEV;break;
       default: ev.event = EVENT_ERROR; break;
     }
 
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,6 +6,8 @@ static Context* do_event(Event e, Context* c) {
       case 2:panic("ev.event = EVENT_SYSCALL");break;
       case 3:panic("ev.event = EVENT_PAGEFAULT"); break;
       case 4:panic("ev.event = EVENT_ERROR");break;
+      case 5:panic("ev.event = EVENT_IRQ_TIMER");break;
+      case 6:panic("ev.event = EVENT_IRQ_IODEV");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
