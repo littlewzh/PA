b443273d60ab876e282773b3977595dc07086ffd
tracer-ics2021 <tracer@njuics.org>
2021-11-23 21:06:52 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 9e8b1a07ffbc404c949e55cb332fef95   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:06:52 up  2:53,  1 user,  load average: 1.47, 1.41, 1.28

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -1,48 +1,54 @@
 def_EHelper(beq){
-    rtl_setrelop(s,2,t0,dsrc1,dsrc2);
+    /*rtl_setrelop(s,2,t0,dsrc1,dsrc2);
     if(*t0){
          //rtl_li(s,t0,id_dest->imm);
         //rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(id_dest->imm);
-    }
+    }*/
+    rtl_jrelop(s,2,dsrc1,dsrc2,(s->pc)+(id_dest->imm));
 }
 def_EHelper(bne){
-    rtl_setrelop(s,3,t0,dsrc1,dsrc2);
+    /*rtl_setrelop(s,3,t0,dsrc1,dsrc2);
     if(*t0){
         //rtl_li(s,t0,id_dest->imm);
         //rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(id_dest->imm);
-    }
+    }*/
+    rtl_jrelop(s,3,dsrc1,dsrc2,(s->pc)+(id_dest->imm));
 }
 def_EHelper(bge) {
-    rtl_setrelop(s,5,t0,dsrc1,dsrc2);
+    /*rtl_setrelop(s,5,t0,dsrc1,dsrc2);
     if(*t0){
         //rtl_li(s,t0,id_dest->imm);
         //rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(id_dest->imm);
-    }
+    }*/
+    rtl_jrelop(s,5,dsrc1,dsrc2,(s->pc)+(id_dest->imm));
 }
 def_EHelper(blt) {
-    rtl_setrelop(s,4,t0,dsrc1,dsrc2);
+    /*rtl_setrelop(s,4,t0,dsrc1,dsrc2);
     if(*t0){
         //rtl_li(s,t0,id_dest->imm);
         //rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(id_dest->imm);
-    }
+    }*/
+    rtl_jrelop(s,4,dsrc1,dsrc2,(s->pc)+(id_dest->imm));
 }
 def_EHelper(bltu) {
-    rtl_setrelop(s,8,t0,dsrc1,dsrc2);
+    /*rtl_setrelop(s,8,t0,dsrc1,dsrc2);
     if(*t0){
         //rtl_li(s,t0,id_dest->imm);
         //rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(id_dest->imm);
-    }
+    }*/
+    rtl_jrelop(s,8,dsrc1,dsrc2,(s->pc)+(id_dest->imm));
 }
 def_EHelper(bgeu) {
-    rtl_setrelop(s,9,t0,dsrc1,dsrc2);
+    /*rtl_setrelop(s,9,t0,dsrc1,dsrc2);
     if(*t0){
         //rtl_li(s,t0,id_dest->imm);
         //rtl_sext(s,t0,t0,12);
         s->dnpc=(s->pc)+(id_dest->imm);
-    }
+    }*/
+    rtl_jrelop(s,9,dsrc1,dsrc2,(s->pc)+(id_dest->imm));
 }
