c0e8dc7a0bee6bc297801a013723e23c22b1bb57
tracer-ics2021 <tracer@njuics.org>
2021-11-24 00:32:41 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: b2090b4ddbfd403c878c6a5c9b4eeb7a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:32:42 up  2:27,  1 user,  load average: 0.52, 0.43, 0.32

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ static Context* do_event(Event e, Context* c) {
       case 1:panic("ev.event = EVENT_YIELD"); break;
       case 2:panic("ev.event = EVENT_SYSCALL");break;
       case 3:panic("ev.event = EVENT_PAGEFAULT"); break;
-      //case 4:panic("ev.event = EVENT_ERROR");break;
+      case 4:panic("ev.event = EVENT_ERROR");break;
       case 5:panic("ev.event = EVENT_IRQ_TIMER");break;
       case 6:panic("ev.event = EVENT_IRQ_IODEV");break;
     default: panic("Unhandled event ID = %d", e.event);
