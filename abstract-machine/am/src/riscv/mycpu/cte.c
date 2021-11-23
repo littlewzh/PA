39f94ea7e8c4854b6fa9f578d39631c55cb7762d
tracer-ics2021 <tracer@njuics.org>
2021-11-23 20:30:25 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 9e8b1a07ffbc404c949e55cb332fef95   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:30:25 up  2:16,  1 user,  load average: 1.59, 1.39, 1.19

--- abstract-machine/am/src/riscv/mycpu/cte.c
+++ abstract-machine/am/src/riscv/mycpu/cte.c
@@ -7,6 +7,9 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 1:ev.event = EVENT_YIELD; break;
+      case 2:ev.event = EVENT_SYSCALL;break;
+      case 3:ev.event = EVENT_PAGEFAULT; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
