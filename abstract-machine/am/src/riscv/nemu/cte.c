4ec366578de8907debcc5c27ed2571ff48aee47f
tracer-ics2021 <tracer@njuics.org>
2021-11-27 21:42:14 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 4952a69096074deb9471d45fd3bf7685   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:42:14 up 24 min,  1 user,  load average: 0.34, 0.47, 0.51

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -3,17 +3,16 @@
 #include <klib.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
-extern void do_syscall(Context *c);
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
       case 1:ev.event = EVENT_YIELD; break;
-      //case 2:ev.event = EVENT_SYSCALL;break;
-      //case 3:ev.event = EVENT_PAGEFAULT; break;
-      //case 4:ev.event = EVENT_ERROR;break;
-      //case 5:ev.event = EVENT_IRQ_TIMER;break;
-      //case 6:ev.event = EVENT_IRQ_IODEV;break;
+      case 2:ev.event = EVENT_SYSCALL;break;
+      case 3:ev.event = EVENT_PAGEFAULT; break;
+      case 4:ev.event = EVENT_ERROR;break;
+      case 5:ev.event = EVENT_IRQ_TIMER;break;
+      case 6:ev.event = EVENT_IRQ_IODEV;break;
       default: ev.event = c->mcause;
     }
     c = user_handler(ev, c);
