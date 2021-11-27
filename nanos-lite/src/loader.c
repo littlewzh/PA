18e8cc0d421e19f36e79890e6c09523b3e7c0eba
tracer-ics2021 <tracer@njuics.org>
2021-11-27 19:57:58 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 8f8e5ea86ca8466a8caf206649c364cb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  19:57:58 up  2:17,  1 user,  load average: 0.78, 0.73, 0.79

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,12 +1,12 @@
 #include <proc.h>
 #include <elf.h>
-#ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
-#else
+//#ifdef __LP64__
+//# define Elf_Ehdr Elf64_Ehdr
+//# define Elf_Phdr Elf64_Phdr
+//#else
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
-#endif
+//#endif
 #define addr 0x83000000
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
@@ -16,7 +16,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //assert(*(uint32_t *)elf->e_ident == 0x7f454c46);
   ramdisk_read((void *)addr, 0, get_ramdisk_size());
 
-  return addr+0x50;
+  return addr;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
