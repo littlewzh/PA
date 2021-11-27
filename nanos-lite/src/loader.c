4303831f1b925ba6d413a46e3d1c09251f0e0c1d
tracer-ics2021 <tracer@njuics.org>
2021-11-27 21:04:20 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 8f8e5ea86ca8466a8caf206649c364cb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:04:20 up  3:23,  1 user,  load average: 1.36, 0.96, 0.81

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,13 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-
+#define ADDR 0x83000000
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  //TODO();
+  ramdisk_read((void *)ADDR, 0,get_ramdisk_size());
+  return ADDR;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -1,7 +1,7 @@
 #include <proc.h>
 
 #define MAX_NR_PROC 4
-
+extern void naive_uload(PCB *pcb, const char *filename);
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -23,7 +23,7 @@ void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
-
+  naive_uload(NULL,NULL);
   // load program here
 
 }
