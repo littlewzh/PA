bcfb16c2334eef629beb91c29a9b6e68031849d2
tracer-ics2021 <tracer@njuics.org>
2021-11-30 00:37:25 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: fbfb893fe51b4c95b6d22aba5cc97565   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:37:25 up  2:18,  1 user,  load average: 0.28, 0.36, 0.33

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case 1:printf("ev.event = EVENT_YIELD\n"); break;
-    case 2:printf("ev.event = EVENT_SYSCALL\n");break;//do_syscall(c);break;
+    case 2:printf("ev.event = EVENT_SYSCALL\n");break;do_syscall(c);break;
     //case 3:printf("ev.event = EVENT_PAGEFAULT\n"); break;
     //case 4:printf("ev.event = EVENT_ERROR\n");break;
     //case 5:printf("ev.event = EVENT_IRQ_TIMER\n");break;
