3b599d0660ce6a3c25412b568a5383bd1d713121
tracer-ics2021 <tracer@njuics.org>
2021-11-30 00:13:15 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: fbfb893fe51b4c95b6d22aba5cc97565   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:13:15 up  1:54,  1 user,  load average: 0.52, 0.31, 0.22

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,7 +17,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   Elf_Ehdr elf;
   ramdisk_read(&elf,0,sizeof(Elf_Ehdr));
-  assert(*(uint32_t *)elf.e_ident == 0x7f454c46);
+  //assert(*(uint32_t *)elf.e_ident == 0x7f454c46);
   Elf_Phdr phlf;
   ramdisk_read(&phlf,elf.e_phoff,elf.e_phentsize);
 
