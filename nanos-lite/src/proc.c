8502c035d7bdccf6f1bbcdc0ae134a19ebcbd76c
tracer-ics2021 <tracer@njuics.org>
2021-11-30 01:27:13 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: fbfb893fe51b4c95b6d22aba5cc97565   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  01:27:13 up  3:08,  1 user,  load average: 0.30, 0.16, 0.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,8 +23,8 @@ void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  //naive_uload(NULL,NULL);
-  // load program here
+  naive_uload(NULL,NULL);
+  //load program here
 
 }
 
