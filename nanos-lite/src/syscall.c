34a7d447f4f9c63cc2c5f2bdc86c095de875d5fd
tracer-ics2021 <tracer@njuics.org>
2021-11-29 22:26:04 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: fbfb893fe51b4c95b6d22aba5cc97565   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:26:04 up 7 min,  1 user,  load average: 0.68, 0.80, 0.48

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,9 +17,9 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   switch (a[0]) {
-    /*case SYS_exit:
+    case SYS_exit:
        halt(a[1]);
-       break;*/
+       break;
     case SYS_yield:
        yield();
        c->GPRx = 0;
