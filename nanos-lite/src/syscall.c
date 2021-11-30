af5501595e62c0a95ee19c2feb3d4afa71986631
tracer-ics2021 <tracer@njuics.org>
2021-11-30 16:42:42 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 40b36c8cdc3e4a94abb2afe01accfd96   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:42:42 up 58 min,  1 user,  load average: 0.50, 0.31, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,13 +17,13 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   switch (a[0]) {
-    case SYS_exit:
+    /*case SYS_exit:
        halt(a[1]);
-       break;
-    /*case SYS_yield:
+       break;*/
+    case SYS_yield:
        yield();
        c->GPRx = 0;
-       break;*/
+       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
