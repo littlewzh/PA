99a35280cbb4713bb385bd6d36c8052a86a236f2
tracer-ics2021 <tracer@njuics.org>
2021-12-02 14:05:08 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5237e0fc2fba440a8928e58378e33f9f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  14:05:08 up  1:16,  1 user,  load average: 1.87, 1.36, 1.11

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -69,17 +69,17 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
   extern uint32_t _end;
-  static uint32_t program_break=0;
-  if(program_break==0){
-    program_break=(uint32_t)&_end;
-    _syscall_(SYS_brk,program_break,0,0);
-  }
+  static uint32_t program_break=(uint32_t)&_end;
+  /*if(program_break==0){
+    program_break=_end;
+    //_syscall_(SYS_brk,program_break,0,0);
+  }*/
   if(_syscall_(SYS_brk,program_break+increment,0,0)==0){
     uint32_t old_break=program_break;
     program_break+=increment;
-    return old_break;
+    return (void *)old_break;
   }
-  else {return -1;}
+  else {return (void *)-1;}
   //return (void *)-1;
 }
 
