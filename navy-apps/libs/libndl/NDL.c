99eb6ec724db266ccec7bb8f36840ad9f1240fbc
tracer-ics2021 <tracer@njuics.org>
2021-12-03 00:54:50 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: cd01ee524aef498eaa23c3edaf04e5ad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:54:50 up  3:26,  1 user,  load average: 0.28, 0.32, 0.26

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
@@ -15,7 +19,9 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+  int fd=open("/dev/events",0,0);
+  read(fd,(void *)buf,len);
+  return 1;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
