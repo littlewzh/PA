06e17c352e9cfcad093d24ea098e7588a6af97e6
tracer-ics2021 <tracer@njuics.org>
2021-12-02 23:55:44 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: cd01ee524aef498eaa23c3edaf04e5ad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  23:55:44 up  2:27,  1 user,  load average: 0.15, 0.27, 0.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -8,6 +8,8 @@
 
 #define NAME(key) \
   [AM_KEY_##key] = #key,
+  
+#define KEYDOWN_MASK 0x8000
 
 static const char *keyname[256] __attribute__((used)) = {
   [AM_KEY_NONE] = "NONE",
@@ -23,6 +25,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+
   return 0;
 }
 
