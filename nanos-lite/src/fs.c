fd9cd963696143d96f67496afe76ca2ebbdc734b
tracer-ics2021 <tracer@njuics.org>
2021-12-02 19:08:44 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 30fec0b71b854711a5d77db3c5fa9c0f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  19:08:44 up 51 min,  1 user,  load average: 0.41, 0.37, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -28,7 +28,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, serial_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},                //?????????shenmoguiyisi
 #include "files.h"
@@ -66,7 +66,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
     ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].offset,size);
   }
   else {
-    file_table[fd].write(buf,file_table[fd].disk_offset+file_table[fd].offset,len);
+    file_table[fd].write(buf,file_table[fd].disk_offset+file_table[fd].offset,len);/// len or size?????
   }
   file_table[fd].offset+=size;
   return size;
