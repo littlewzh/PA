a901e09ce7ca0630280ba3d6c6b8c9c64059936b
tracer-ics2021 <tracer@njuics.org>
2021-12-03 00:37:51 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: cd01ee524aef498eaa23c3edaf04e5ad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:37:51 up  3:09,  1 user,  load average: 0.29, 0.29, 0.24

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -1,4 +1,4 @@
-#include <stdint.h>
+/*#include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/time.h>
@@ -10,7 +10,29 @@ int main() {
    while (1) {
   //printf("hello\n");
     while( t1.tv_usec/ 1000000 < sec){gettimeofday(&t1,NULL);}
-    gettimeofday(&t1,NULL);
+    if (sec == 1) {
+      printf("%d second.\n", sec);
+    } else {
+      printf("%d seconds.\n", sec);
+    }
+    sec ++;
+  }
+}*/
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <NDL.h>
+int main() {
+   //struct timeval t1;
+   //struct timezone t2;
+   //t1->tv_usec=; 
+   int sec = 1;
+   uint32_t time=0;
+   while (1) {
+  //printf("hello\n");
+    while( time/ 1000 < sec){time=NDL_GetTicks();}
+    //gettimeofday(&t1,NULL);
     if (sec == 1) {
       printf("%d second.\n", sec);
     } else {
