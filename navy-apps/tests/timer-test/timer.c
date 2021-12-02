ff035ee8d712cf3b1a2f4eba10adc0b5a3397f33
tracer-ics2021 <tracer@njuics.org>
2021-12-02 22:10:20 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: cd01ee524aef498eaa23c3edaf04e5ad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:10:20 up 42 min,  1 user,  load average: 0.32, 0.27, 0.28

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -4,13 +4,13 @@
 #include <sys/time.h>
 int main() {
    struct timeval t1;
-   struct timezone t2;
+   //struct timezone t2;
    //t1->tv_usec=; 
    int sec = 1;
    while (1) {
   //printf("hello\n");
     while( t1.tv_usec/ 1000000 < sec) ;
-    gettimeofday(&t1,&t2);
+    gettimeofday(&t1,NULL);
     if (sec == 1) {
       printf("%d second).\n", sec);
     } else {
