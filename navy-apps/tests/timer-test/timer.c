d64c593e1e1004375075722145849246a112959a
tracer-ics2021 <tracer@njuics.org>
2021-12-02 22:12:52 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: cd01ee524aef498eaa23c3edaf04e5ad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:12:52 up 44 min,  1 user,  load average: 0.10, 0.24, 0.27

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -9,12 +9,12 @@ int main() {
    int sec = 1;
    while (1) {
   //printf("hello\n");
-    while( t1.tv_usec/ 1000000 < sec) ;
+    while( t1.tv_usec/ 1000000 < sec){gettimeofday(&t1,NULL);}
     gettimeofday(&t1,NULL);
     if (sec == 1) {
-      printf("%d second).\n", sec);
+      printf("%d second.\n", sec);
     } else {
-      printf("%d seconds).\n", sec);
+      printf("%d seconds.\n", sec);
     }
     sec ++;
   }
