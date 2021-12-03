08155ff6979d6e30aeee61a228c4d20f1a0d62e4
tracer-ics2021 <tracer@njuics.org>
2021-12-03 16:33:53 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: cf6fefb13a72455ba203a315ed271c37   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:33:53 up  3:56,  1 user,  load average: 1.21, 1.06, 0.97

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -52,7 +52,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   x=(offset/4)%screen_w;
   y=(offset/4)/screen_w;
   //for(int )
-  io_write(AM_GPU_FBDRAW, x, y,(char *)buf, 128, 128, true);
+  io_write(AM_GPU_FBDRAW, x, y,(char *)buf, 400, 300, true);
   return len;
 }
 
