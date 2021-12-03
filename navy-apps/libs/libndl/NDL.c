d22d0a0948b47689119ebb55d5e2399991dba42f
tracer-ics2021 <tracer@njuics.org>
2021-12-03 22:32:26 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: afb184e7e4324b3d9f810328fe3e3327   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  22:32:26 up 23 min,  1 user,  load average: 0.79, 0.80, 0.70

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -58,7 +58,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd=open("/dev/fb",0,0);
   //for(int i=0;i<h;i++){
-    
+    printf("reach here\n");
   //}
     uint32_t  canvas[300][400]={0};
     //memset(canvas,0,sizeof(canvas));
@@ -68,7 +68,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
         //canvas[(i + y) * 400 + (j + x)] = pixels[i * w + j];
       }
     }
-    printf("reach here");
+    printf("reach here\n");
     write(fd,(void *)canvas,400*300*4);
 }
 
