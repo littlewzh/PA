c95a28fdd2b31c5a85cfa36fca583bd01972fe6e
tracer-ics2021 <tracer@njuics.org>
2021-12-04 01:46:28 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: afb184e7e4324b3d9f810328fe3e3327   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  01:46:28 up  3:37,  1 user,  load average: 0.40, 0.55, 0.55

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,7 +17,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  char *buf;
+  /*char *buf;
   NDL_PollEvent(buf, 8);
   if(*(buf+1)=='d'){
     event->type=SDL_KEYDOWN;
@@ -27,7 +27,7 @@ int SDL_WaitEvent(SDL_Event *event) {
     event->type=SDL_KEYUP;
     
   }
-  else {event->type=2;}
+  else {event->type=2;}*/
   return 1;
 }
 
