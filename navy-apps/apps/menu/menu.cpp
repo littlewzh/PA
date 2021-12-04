56bdefd2e4ce4fc8e23628df8d0cfd0a1bd7a35b
tracer-ics2021 <tracer@njuics.org>
2021-12-04 13:57:34 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5050ee72613e4134919ad5b0bcca7943   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  13:57:34 up  3:15,  1 user,  load average: 0.49, 0.37, 0.19

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -79,7 +79,7 @@ int main(int argc, char *argv[], char *envp[]) {
   printf("reach here3\n");
   while (1) {
     display_menu(i_max);
-    printf("reach here4\n");
+    
     SDL_Event e;
     do {
       SDL_WaitEvent(&e);
@@ -143,7 +143,8 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+  printf("reach here4\n");
+  SDL_BlitSurface(logo_sf, &rect ,screen, NULL);
   printf("Available applications:\n");
   char buf[80];
   int i;
