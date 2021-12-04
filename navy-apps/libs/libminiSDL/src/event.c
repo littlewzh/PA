76a01f1c773b9e497ffa0a73e6eceeec9989f40d
tracer-ics2021 <tracer@njuics.org>
2021-12-04 11:20:06 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5050ee72613e4134919ad5b0bcca7943   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:20:06 up 38 min,  1 user,  load average: 0.58, 0.68, 0.53

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -19,7 +19,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 
 int SDL_WaitEvent(SDL_Event *event) {
   
-  if(NDL_PollEvent(buf, 64)){
+  if(NDL_PollEvent(buf, sizeof(buf))){
       //sscanf(buf,"%d",(int *)&num);
   //int f=*num;
   //printf("%d",f);
@@ -27,11 +27,11 @@ int SDL_WaitEvent(SDL_Event *event) {
     event->type=SDL_KEYDOWN;
     event->key.keysym.sym=num;
     //printf("%d\n",event->key.keysym.sym);
-    //printf("%d\n",event->type);
+    printf("%d\n",event->type);
   }
   else if(buf[1]=='u'){
     event->type=SDL_KEYUP;
-    //printf("%d\n",event->type);
+    printf("%d\n",event->type);
   }
   else {event->type=2;}
   return 1;
