ae24a40f819b25ebf92377ec62b73d00eeec6434
tracer-ics2021 <tracer@njuics.org>
2021-12-04 14:13:22 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5050ee72613e4134919ad5b0bcca7943   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  14:13:22 up  3:31,  1 user,  load average: 0.48, 0.46, 0.41

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -33,7 +33,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},                //?????????shenmoguiyisi
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},                //?????????shenmoguiyisi
 #include "files.h"
   {"/dev/events", 0, 0, 0, events_read, invalid_write},
   {"/proc/dispinfo", 0, 0, 0,dispinfo_read, invalid_write},
