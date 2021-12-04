65a9b7a6d7bfe3f1dd1b204ec182dcc834be9ef5
tracer-ics2021 <tracer@njuics.org>
2021-12-04 12:38:11 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 5050ee72613e4134919ad5b0bcca7943   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  12:38:11 up  1:56,  1 user,  load average: 0.25, 0.32, 0.25

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,11 +7,16 @@
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  NDL_DrawRect(src->pixels, dstrect->x, dstrect->y,dstrect->w,dstrect->h);
+  //NDL_DrawRect(src->pixels, dstrect->x, dstrect->y,dstrect->w,dstrect->h);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  NDL_DrawRect(dst->pixels, dstrect->x, dstrect->y,dstrect->w,dstrect->h);
+  for(int i=0;i<dstrect->h;i++){
+    for(int j=0;j<dstrect->w;j++){
+      *(dst->pixels)=color;
+    }
+  }
+  //NDL_DrawRect(dst->pixels, dstrect->x, dstrect->y,dstrect->w,dstrect->h);
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
