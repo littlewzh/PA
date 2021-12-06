68de6dd8d596ce16f8811566a0608c226de7a65c
tracer-ics2021 <tracer@njuics.org>
2021-12-06 16:14:47 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f278a5680c1e44c18f856741a6a05336   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:14:47 up 52 min,  1 user,  load average: 1.37, 1.29, 1.19

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+static uint32_t *d;
+static uint32_t *s;
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   //assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  uint32_t* d=dst->pixels;
-  uint32_t* s=src->pixels;
+  d=dst->pixels;
+  s=src->pixels;
 
   if(srcrect==NULL){
      printf("reach here\n");
