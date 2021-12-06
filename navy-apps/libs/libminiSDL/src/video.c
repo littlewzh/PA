788d99746d63563820b605a7344652621b123820
tracer-ics2021 <tracer@njuics.org>
2021-12-06 16:16:46 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f278a5680c1e44c18f856741a6a05336   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:16:46 up 54 min,  1 user,  load average: 1.66, 1.45, 1.26

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -4,8 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-static uint32_t *d;
-static uint32_t *s;
+static uint32_t *d=0;
+static uint32_t *s=0;
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   //assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
