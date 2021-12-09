4b2553e1881264e4df13de4a41b93b0c1a85b527
tracer-ics2021 <tracer@njuics.org>
2021-12-09 15:53:44 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: b741c6a00fe04bb2bb0979f6a28acfac   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  15:53:44 up  5:38,  1 user,  load average: 0.60, 0.82, 0.87

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -41,7 +41,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 static char dispinfo[128] __attribute__((used)) = {};
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  sprintf(buf,"%s",dispinfo);
+  sprintf(buf,"WIDTH");
   //strncpy((char *)buf,(char *)(dispinfo),len);
   //ramdisk_read(buf,offset,len);
   return len;
