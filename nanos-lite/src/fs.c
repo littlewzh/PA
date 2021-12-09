fca40063143825b9fce372e404ac4c5f91d58b0b
tracer-ics2021 <tracer@njuics.org>
2021-12-09 13:21:53 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: b741c6a00fe04bb2bb0979f6a28acfac   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  13:21:53 up  3:07,  1 user,  load average: 0.75, 0.79, 0.75

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,7 +36,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},                //?????????shenmoguiyisi
 #include "files.h"
   {"/dev/events", 0, 0, 0, events_read, invalid_write},
-  {"/proc/dispinfo", 0, 0, 0,dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 0, 0, 0,NULL, invalid_write},
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
 };
 #define fs_number (sizeof(file_table)/sizeof(Finfo))
