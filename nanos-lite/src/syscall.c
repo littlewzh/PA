c25d423cb0c3c7e2a3e56111d6ae75b32ebb655b
tracer-ics2021 <tracer@njuics.org>
2021-12-13 16:18:11 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: d56fe6a4e614491bb68dc97c3ea52708   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:18:11 up 57 min,  1 user,  load average: 0.66, 0.60, 0.48

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -47,7 +47,7 @@ void do_syscall(Context *c) {
     
     //#endif
     case SYS_exit:
-       naive_uload(NULL,"/bin/nterm");
+       naive_uload(NULL,"/bin/menu");
        //halt(a[1]);
        break;
     case SYS_yield:
