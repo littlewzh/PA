4dbfbede2b8793de006ceec3fe8d4ac78537fca2
tracer-ics2021 <tracer@njuics.org>
2021-12-16 11:54:05 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f6b51d5476b342889c6faae1c6c1c8a5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:54:05 up  1:44,  1 user,  load average: 0.88, 0.95, 0.99

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -4,14 +4,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-//static uint32_t *d=0;
-//static uint32_t *s=0;
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
   uint32_t *d=(uint32_t *)dst->pixels;
   uint32_t *s=(uint32_t *)src->pixels;
-  //d=dst->pixels+dstrect->y*400+dstrect->x;
   int x,y;
   if((dstrect)==NULL) {x=0;y=0;}
   else {x=dstrect->x;y=dstrect->y;}
@@ -39,7 +36,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
    }
   }
 }
-//static uint32_t *d;
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   uint32_t *d=(uint32_t *)dst->pixels;
  if(dstrect==NULL){
