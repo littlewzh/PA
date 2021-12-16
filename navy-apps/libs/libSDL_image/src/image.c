4dce64daad0ede17068c1248ac6659604324ed84
tracer-ics2021 <tracer@njuics.org>
2021-12-16 16:24:42 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f6b51d5476b342889c6faae1c6c1c8a5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:24:43 up  6:14,  1 user,  load average: 0.97, 1.02, 0.94

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -17,8 +17,9 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(freesrc == 0);
   return NULL;
 }
-static char buf[400*300*4]={0};
-SDL_Surface* IMG_Load(const char *filename) {
+//static char buf[400*300*4]={0};
+
+/*SDL_Surface* IMG_Load(const char *filename) {
   printf("reach img_load\n");
   int fd=open(filename,0,0);
   //char *buf;
@@ -28,8 +29,20 @@ SDL_Surface* IMG_Load(const char *filename) {
   size=read(fd,(void *)buf,size);
   return STBIMG_LoadFromMemory(buf,size);
   //return NULL;
+}*/
+SDL_Surface* IMG_Load(const char *filename) {
+  FILE* fp = fopen(filename,"r");
+  fseek(fp,0,SEEK_END);
+  size_t size = ftell(fp);   
+  char* buf = malloc(size);
+  memset(buf,0,size);
+  fseek(fp,0,SEEK_SET);
+  fread(buf,1,size,fp);
+  SDL_Surface* sp = STBIMG_LoadFromMemory(buf,size);
+  free(buf);
+  fclose(fp);
+  return sp;
 }
-
 int IMG_isPNG(SDL_RWops *src) {
   return 0;
 }
