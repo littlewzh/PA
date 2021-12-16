e3f09c198d30271bf4876cd2f32e3a89045ae58f
tracer-ics2021 <tracer@njuics.org>
2021-12-16 13:43:55 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f6b51d5476b342889c6faae1c6c1c8a5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  13:43:55 up  3:33,  1 user,  load average: 0.62, 0.59, 0.41

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -30,8 +30,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     for(int j=0;j<srcrect->w;j++){
       if(dst->format->BitsPerPixel==8){
         printf("blits\n");
-        //*(dst->pixels+(i+y)*(dst->w)+j+x)=*(src->pixels+(i+srcrect->y)*src->w+j+srcrect->x);
-        memcpy(dst->pixels+(y+i)*dst->w+(x+j),src->pixels+(i+srcrect->y)*src->w+j+srcrect->x,src->format->BytesPerPixel);
+        *(dst->pixels+(i+y)*(dst->w)+j+x)=*(src->pixels+(i+srcrect->y)*src->w+j+srcrect->x);
+        //memcpy(dst->pixels+(y+i)*dst->w+(x+j),src->pixels+(i+srcrect->y)*src->w+j+srcrect->x,src->format->BytesPerPixel);
       }
       else {*(d+(i+y)*(dst->w)+j+x)=*(s+(i+srcrect->y)*src->w+j+srcrect->x);}
      }
