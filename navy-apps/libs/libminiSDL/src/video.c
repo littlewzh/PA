fd225dc1a110a3aa079429d6976477ca492d75a5
tracer-ics2021 <tracer@njuics.org>
2021-12-17 11:39:20 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 4ef938c94eaa4c0c830efde0c05d35e0   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:39:21 up  1:36,  1 user,  load average: 1.52, 1.90, 1.59

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -74,7 +74,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   else{
     printf("reach update2\n");
     //pix=(uint32_t *)s->pixels;
-     if(x==0&&y==0&&w==0&&h==0) {NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);}
+     if(w==0||h==0) {NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);}
      else {NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);}
   }
 }
