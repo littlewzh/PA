c2c8548476a92545e481f70350bf8f0b8e8cd853
tracer-ics2021 <tracer@njuics.org>
2022-01-11 18:11:58 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 1a2da2ee32e74530af846b9f458a6462   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  18:11:58 up  8:19,  1 user,  load average: 0.35, 0.29, 0.19

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -18,8 +18,8 @@ void context_uload(PCB *pcb,const char *filename){
   pcb->cp = ucontext(NULL,kstack,(void *)entry);
   //printf("%x\n",&kstack.start);
   //printf("%x\n",&kstack.end);
-  //printf("heap.start=%x\n",heap.start);
-  //printf("heap.end=%x\n",heap.end);
+  printf("heap.start=%x\n",heap.start);
+  printf("heap.end=%x\n",heap.end);
 }
 void context_kload(PCB *pcb,void (*entry)(void *), void *arg){
   Area kstack;
