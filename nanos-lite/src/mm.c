78d6312e4210f52a88b4e812b040bbf3ad017ee1
tracer-ics2021 <tracer@njuics.org>
2022-01-12 11:41:16 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 291a8b9f97ba4dedbf37ac519dee331e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:41:16 up  4:00,  1 user,  load average: 0.25, 0.20, 0.11

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -3,7 +3,9 @@
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
-  return NULL;
+  uint8_t *new = (uint8_t *)pf;
+  pf = (uint8_t *)pf + nr_page*PGSIZE;
+  return (void *)new;
 }
 
 #ifdef HAS_VME
@@ -22,7 +24,7 @@ int mm_brk(uintptr_t brk) {
 }
 
 void init_mm() {
-  pf = (void *)ROUNDUP(heap.start, PGSIZE);
+  pf = (void *)ROUNDUP(heap.start, PGSIZE);           //fenpei 32KB memory from heap.start
   Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -3,6 +3,7 @@
 #define MAX_NR_PROC 4
 extern void naive_uload(PCB *pcb, const char *filename);
 extern uintptr_t loader(PCB *pcb, const char *filename);
+extern void* new_page(size_t nr_page);
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -11,6 +12,33 @@ void switch_boot_pcb() {
   current = &pcb_boot;
 }
 void context_uload(PCB *pcb,const char *filename, char *const argv[], char *const envp[]){
+  uint32_t start,sp;
+  start=(uint32_t)new_page(8);  //表示用户栈的开始
+  sp=start+31*1024;               //栈指针，并将最高的1KB设置为unspecified区域
+  int num;
+  char *uenvp[16];
+  for(num=0;envp[num]!=0;num++) {
+		sp-=strlen(envp[num])+1;
+		memcpy((void *)sp, (void *)(envp[num]), strlen(envp[num]) + 1);
+		uenvp[num]=(char *)sp;
+	}
+  uenvp[num]=0;                   //
+  int argc;
+  char *uargv[16];
+	for(argc=0;argv[argc]!=0;argc++) {
+		sp-=strlen(argv[argc])+1;
+		memcpy((void *)sp, (void *)(argv[argc]), strlen(argv[argc]) + 1);
+		uargv[argc]=(char *)sp;
+	}
+	uargv[argc]=0;                  //
+	sp-=1024;                        //unspecified区域
+	sp-=(num+1)*sizeof(uint32_t);
+	//sp-=sp % 16;
+	memcpy((void *)sp, (void *)uenvp, sizeof(uint32_t) * (num + 1));
+	sp-=(argc + 1)*sizeof(uint32_t);
+	memcpy((void *)sp, (void *)uargv, sizeof(uint32_t) * (argc + 1));
+	sp-=sizeof(uint32_t);
+	memcpy((void *)sp, (void *)&argc, sizeof(uint32_t));
   Area ustack;
   //ustack.end=heap.end;
   //ustack.start=ustack.end-sizeof(pcb->stack);
@@ -18,8 +46,7 @@ void context_uload(PCB *pcb,const char *filename, char *const argv[], char *cons
   ustack.end = ustack.start + sizeof(pcb->stack);
   uintptr_t entry = loader(pcb, filename);
   pcb->cp = ucontext(NULL,ustack,(void *)entry);
-
-  pcb->cp->gpr[10]=(uintptr_t)heap.end-4;
+  pcb->cp->gpr[10]=(uintptr_t)sp;
   //printf("%x\n",&kstack.start);
   //printf("%x\n",&kstack.end);
   //printf("heap.start=%x\n",heap.start);
