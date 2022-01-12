f82eccbce12e88000c823b270ba6a4e457480434
tracer-ics2021 <tracer@njuics.org>
2022-01-12 11:47:11 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 291a8b9f97ba4dedbf37ac519dee331e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:47:11 up  4:06,  1 user,  load average: 0.55, 0.29, 0.16

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
+
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
@@ -8,6 +10,7 @@ void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   int argc;
   argc=*args;
+  printf("argc = %d\n",argc);
   char* argv[argc];
   for(int i=0;i<argc;i++){
      argv[i]=(char *)*(args+4+i*4);
@@ -19,6 +22,6 @@ void call_main(uintptr_t *args) {
      envp[i]=(char *)*(args+4+4*argc+i*4);
   }
   environ = envp;
-  exit(main(0, empty, empty));
+  exit(main(argc, empty, empty));
   assert(0);
 }
