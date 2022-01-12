87efd96f81d3389bd7b7d7c796f20f4445629fa8
tracer-ics2021 <tracer@njuics.org>
2022-01-12 21:08:38 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 291a8b9f97ba4dedbf37ac519dee331e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:08:38 up 13:28,  1 user,  load average: 0.59, 0.43, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,7 +13,9 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
-uintptr_t loader(PCB *pcb, const char *filename) {
+extern void* new_page(size_t nr_page);
+extern Area heap;
+static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   /*Elf_Ehdr elf;
   ramdisk_read(&elf,0,sizeof(Elf_Ehdr));
@@ -53,3 +55,52 @@ void naive_uload(PCB *pcb, const char *filename) {
   ((void(*)())entry) ();
 }
 
+void context_uload(PCB *pcb,const char *filename, char *const argv[], char *const envp[]){
+  uint32_t start,sp;
+  start=(uint32_t)new_page(8);  //表示用户栈的开始
+  sp=start+31*1024;               //栈指针，并将最高的1KB设置为unspecified区域
+  if(argv!=NULL&&envp!=NULL){
+  int num;
+  char *uenvp[16];
+  for(num=0;envp[num]!=0;num++) {
+		sp-=strlen(envp[num])+1;
+		memcpy((void *)sp, (void *)(envp[num]), strlen(envp[num]) + 1);
+		uenvp[num]=(char *)sp;
+	}
+  uenvp[num]=0;                   //
+  int argc;
+  char *uargv[16];
+	for(argc=0;argv[argc]!=0;argc++) {
+		sp-=strlen(argv[argc])+1;
+		memcpy((void *)sp, (void *)(argv[argc]), strlen(argv[argc]) + 1);
+		uargv[argc]=(char *)sp;
+	}
+	uargv[argc]=0;                  //
+	sp-=1024;                        //unspecified区域
+	sp-=(num+1)*sizeof(uint32_t);
+	sp-=sp % 4;                      //对齐
+	memcpy((void *)sp, (void *)uenvp, sizeof(uint32_t) * (num + 1));
+  printf("envp = %p\n",sp);
+	sp-=(argc + 1)*sizeof(uint32_t);
+	memcpy((void *)sp, (void *)uargv, sizeof(uint32_t) * (argc + 1));
+  printf("argv = %p\n",sp);
+	sp-=sizeof(uint32_t);
+	memcpy((void *)sp, (void *)&argc, sizeof(uint32_t));
+  printf("&argc = %p\n",sp);
+  }
+  
+  Area ustack;
+  //ustack.end=heap.end;
+  //ustack.start=ustack.end-sizeof(pcb->stack);
+  ustack.start= pcb->stack;
+  ustack.end = ustack.start + sizeof(pcb->stack);
+  assert(filename!=NULL);
+  uintptr_t entry = loader(pcb, filename);
+  pcb->cp = ucontext(NULL,ustack,(void *)entry);
+  pcb->cp->gpr[10]=(uintptr_t)sp;
+  printf("finish\n");
+  //printf("%x\n",&kstack.start);
+  //printf("%x\n",&kstack.end);
+  //printf("heap.start=%x\n",heap.start);
+  //printf("heap.end=%x\n",heap.end);
+}
