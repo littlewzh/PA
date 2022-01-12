9aa31af27df257a9e905841abfa512c4eddf1700
tracer-ics2021 <tracer@njuics.org>
2022-01-12 20:41:51 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 291a8b9f97ba4dedbf37ac519dee331e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:41:51 up 13:01,  1 user,  load average: 0.34, 0.34, 0.41

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -11,12 +11,16 @@ void call_main(uintptr_t *args) {
   printf("args = %p\n",args);
   int argc;
   argc=*args;
-  printf("argc = %d\n",argc);
-  char **argv,**envp;
-  argv=args+1;
-  printf("argv = %p\n",argv);
-  envp=args+(2+argc);
-  printf("envp = %p\n",envp);
-  exit(main(argc, argv, envp));
+  if(argc!=0){
+    printf("argc = %d\n",argc);
+    char **argv,**envp;
+    argv=args+1;
+    printf("argv = %p\n",argv);
+    envp=args+(2+argc);
+    printf("envp = %p\n",envp);
+    exit(main(argc, argv, envp));
+  }
+  else {exit(main(0,empty,empty));}
+  
   assert(0);
 }
