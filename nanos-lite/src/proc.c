33cfbc45b0284616055fb90afcb9ba9c7579353e
tracer-ics2021 <tracer@njuics.org>
2022-01-12 11:43:08 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 291a8b9f97ba4dedbf37ac519dee331e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:43:09 up  4:02,  1 user,  load average: 0.33, 0.23, 0.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -72,7 +72,9 @@ void hello_fun(void *arg) {
 void init_proc() {
   //context_uload(&pcb[0], "/bin/hello");
   context_kload(&pcb[0], hello_fun, (void *)8000);
-  context_uload(&pcb[1], "/bin/pal",NULL,NULL);
+  char *environ[] ={NULL };
+	char *args[] = {NULL };
+  context_uload(&pcb[1], "/bin/pal",args,environ);
   //context_kload(&pcb[1], hello_fun, (void *)1000);
   switch_boot_pcb();
 
