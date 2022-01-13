ffc64b683d06d6eb7488d00d98df6f08865a75cc
tracer-ics2021 <tracer@njuics.org>
2022-01-13 23:07:15 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f561e46f2b0641789b869dd3d464d681   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  23:07:15 up  1:26,  1 user,  load average: 0.31, 0.41, 0.35

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -10,7 +10,7 @@
  * You can modify this value as you want.
  */
 #define MAX_INSTR_TO_PRINT 1000
-#define HAS_TIMER_IRQ
+//#define HAS_TIMER_IRQ
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_instr = 0;
@@ -115,9 +115,10 @@ void cpu_exec(uint64_t n) {
     word_t intr = isa_query_intr();
     if (intr != INTR_EMPTY) {
     cpu.pc = isa_raise_intr(intr, cpu.pc);
+    }
     #endif
     //
-}
+
   }
 
   uint64_t timer_end = get_time();
