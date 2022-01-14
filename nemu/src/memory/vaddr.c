687cdd0f80a343cfdbb3010e6c22d0e776f5aa06
tracer-ics2021 <tracer@njuics.org>
2022-01-14 14:37:52 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 7e241f06286f4bd08576569045b81db6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  14:37:52 up  5:11,  1 user,  load average: 0.26, 0.36, 0.28

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -2,13 +2,32 @@
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(isa_mmu_check(addr,len,1)==0){
+    return paddr_read(addr, len);
+  }
+  else{
+    return paddr_read(isa_mmu_translate(addr,len,1),len);
+  }
+  //return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(isa_mmu_check(addr,len,1)==0){
+    return paddr_read(addr, len);
+  }
+  else{
+    return paddr_read(isa_mmu_translate(addr,len,1),len);
+  }
+  //return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_write(addr, len, data);
+
+  //paddr_write(addr, len, data);
+  if(isa_mmu_check(addr,len,1)==0){
+    return paddr_write(addr, len,data);
+  }
+  else{
+    return paddr_write(isa_mmu_translate(addr,len,1),len,data);
+  }
 }
