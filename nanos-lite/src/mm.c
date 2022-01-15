a1be9a52a12924804aec045b68843ee6cb5c5ab1
tracer-ics2021 <tracer@njuics.org>
2022-01-15 12:47:33 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 86fcca3d85b4451497c9e6705114e472   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  12:47:33 up  2:51,  1 user,  load average: 0.60, 0.34, 0.34

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -25,18 +25,15 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-   /*if(brk > (current->max_brk)){
+   if(brk > (current->max_brk)){
       uint32_t tem = (current->max_brk)&0xfffff000;
       while(tem < brk){
         void* paddr = new_page(1);
-        
-        
         map(&current->as,(void*)(current->max_brk),paddr,0);
-        
         tem += 4096;
         (current->max_brk) += 4096;
       }
-  }*/
+  }
   return 0;
 }
 
