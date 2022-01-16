aeba86331b8b05caeab13b5143b89770ba72314c
tracer-ics2021 <tracer@njuics.org>
2022-01-16 09:10:30 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f2186c6043904054afde8caf3a9cafe6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  09:10:30 up 47 min,  1 user,  load average: 0.27, 0.27, 0.26

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -10,13 +10,13 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mcause=NO;
   cpu.mepc=epc;
   //printf("%x\n",cpu.mtvec);
-  cpu.mstatus=(cpu.mstatus|0x8)<<4;                                   //MIE(3) MPIE(7)  
+  cpu.mstatus=(cpu.mstatus<<4)&0x80;                                   //MIE(3) MPIE(7)  
 
   return cpu.mtvec;
 }
 
 word_t isa_query_intr() {
-   if (cpu.INTR==1&&((cpu.mstatus>>3)&1)) {
+   if (cpu.INTR==1&&((cpu.mstatus&0x8)!=0)&&((cpu.mstatus&0x80)!=0)) {
     cpu.INTR = false;
     return IRQ_TIMER;
   }
