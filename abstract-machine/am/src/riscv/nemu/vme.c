d921aa4bf90724124edafeb6cbc0ba043a5fab12
tracer-ics2021 <tracer@njuics.org>
2022-01-16 10:59:18 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f2186c6043904054afde8caf3a9cafe6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  10:59:18 up  2:36,  1 user,  load average: 0.40, 0.28, 0.12

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -85,7 +85,7 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
    memset(text,0,sizeof(Context));
    //text->gpr[10]=(uintptr_t)heap.end;
    text->mepc=(uintptr_t)entry;
-   text->mstatus=0x8;
+   text->mstatus=0x80;
    text->pdir=(void *)as->ptr;
    return text;
   //return NULL;
