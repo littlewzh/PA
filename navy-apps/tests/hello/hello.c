76496fcf61c4b446b66080483e4ea7bc3d169de4
tracer-ics2021 <tracer@njuics.org>
2022-01-16 09:19:35 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f2186c6043904054afde8caf3a9cafe6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  09:19:35 up 56 min,  1 user,  load average: 0.46, 0.35, 0.28

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -6,13 +6,13 @@ int main() {
   int i = 2;
   uint32_t p=&i;
   printf("0x%08x\n",p);
-  //volatile int j = 0;
- // while (1) {
-   // j ++;
-   // if (j == 1000000) {
+  volatile int j = 0;
+  while (1) {
+    j ++;
+    if (j == 1000000) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-   //   j = 0;
-  //  }
- //}
+      j = 0;
+     }
+ }
   return 0;
 }
