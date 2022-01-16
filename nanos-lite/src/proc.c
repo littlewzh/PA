8652b11c6d7ae77441ed1f308b69267bc80e06da
tracer-ics2021 <tracer@njuics.org>
2022-01-16 12:39:48 +0800

    >  compile 201240080    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: f2186c6043904054afde8caf3a9cafe6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  12:39:49 up  4:16,  1 user,  load average: 0.52, 0.29, 0.34

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,14 +32,14 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   //context_uload(&pcb[0], "/bin/hello");
-  //context_kload(&pcb[1], hello_fun, NULL);
+  context_kload(&pcb[1], hello_fun, NULL);
   //context_kload(&pcb[0], hello_fun, "A");
   char* empty[] ={NULL};
   //char *environ[] ={NULL };
 	char *args[] = {"--skip", NULL};
   //context_uload(&pcb[0], "/bin/exec-test",empty,empty);
   context_uload(&pcb[0], "/bin/pal",args,empty);
-  context_uload(&pcb[1], "/bin/hello",args,empty);
+  //context_uload(&pcb[1], "/bin/hello",args,empty);
   //context_uload(&pcb[0], "/bin/nterm",args,empty);
   //context_kload(&pcb[1], hello_fun, (void *)1000);
   switch_boot_pcb();
